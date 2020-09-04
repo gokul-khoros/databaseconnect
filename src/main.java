e760a6779c2ea7b759be8a7c520ef89989ad5492
Gokul_khoros <gokulreko@gmail.com>
2020-09-04 20:55:47 +0530

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+public class main {
+    public static void main(String[] args) throws SQLException {
+
+
+        try {
+            Connection conc = DriverManager.getConnection("jdbc:mysql://localhost:3306/heros", "root", "Goksravi99.");
+            /*Statement stat = conc.createStatement();
+            ResultSet rst = stat.executeQuery();
+            */
+        }
+        catch (SQLException err)
+        {
+            System.out.println(err.getMessage());
+        }
+
+
+    }
+}
